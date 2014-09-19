8445fcdda6499f9bbd688522b93f5d0d3f303e29
Josh Jordan <josh.jordan@gmail.com>
2014-09-19 00:12:16 -0400

    WIP on stats

--- collector.rb
+++ collector.rb
@@ -7,28 +7,52 @@ module Rack
 
       def stop
         trace_point.disable
+        allocation_stats.stop
       end
 
-      def counts
-        @counts ||= Hash.new(0)
+      def statistics
+        @statistics ||= call_counts.dup.tap do |statistics|
+          statistics[:allocations] = allocation_stats.allocations.count
+          statistics[:allocated_memory] = allocation_stats.allocations.sum(&:memsize)
+          allocation_stats.allocations.group_by(:class).each do |klass, allocations|
+            statistics[klass] = {} if !statistics.has_key? klass
+            statistics[klass][:allocations] = allocations.count
+            statistics[klass][:allocated_memory] = allocations.sum(&:memsize)
+          end
+        end
       end
 
       private
 
-      CALL_EVENTS = [:call, :c_call]
-      attr_reader :trace_point
+      attr_reader :trace_point, :allocation_stats
+
+      def call_counts
+        @call_counts ||= Hash.new(0)
+      end
 
       def begin_trace
+        begin_call_trace
+        begin_allocation_trace
+      end
+
+      CALL_EVENTS = [:call, :c_call]
+
+      def begin_call_trace
         @trace_point = TracePoint.trace(:call, :c_call, :raise) do |t|
           if CALL_EVENTS.include? t.event
-            counts[:total_calls] += 1
-            counts[t.defined_class] = Hash.new(0) if counts[t.defined_class] == 0
-            counts[t.defined_class][:total_calls] += 1
-            counts[t.defined_class][t.method_id] += 1
+            call_counts[:total_calls] += 1
+            call_counts[t.defined_class] = Hash.new(0) if !call_counts.has_key? t.defined_class
+            call_counts[t.defined_class][:total_calls] += 1
+            call_counts[t.defined_class][t.method_id] += 1
           elsif t.event == :raise
-            counts[:exceptions] += 1
+            call_counts[:exceptions] += 1
           end
         end
+      end
+
+      def begin_allocation_trace
+        @allocation_stats = AllocationStats.trace
+        puts "beginning trace"
       end
     end
   end
